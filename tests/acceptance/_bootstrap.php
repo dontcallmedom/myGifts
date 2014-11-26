cb9036f573ce052c3ddab88bf6c7dbb057bb1950
Dominique Hazael-Massieux <dom@w3.org>
2014-11-26 23:14:01 +0100

    clean before testing

--- tests/acceptance/_bootstrap.php
+++ tests/acceptance/_bootstrap.php
@@ -1,3 +1,7 @@
 <?php
 // Here you can initialize variables that will be available to your tests
 @unlink("config/config.inc.php");
+$mysqli = new mysqli("localhost", "mygifts_dev", "mygifts_dev", "mygifts_dev");
+$mysqli->query("DROP DATABASE mygifts_dev");
+$mysqli->query("CREATE DATABASE mygifts_dev");
+
