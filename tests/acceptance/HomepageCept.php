addc98a1dc37272befcd9becbb0341a6330a241e
Dominique Hazael-Massieux <dom@w3.org>
2014-11-26 23:13:46 +0100

    more tests

--- tests/acceptance/HomepageCept.php
+++ tests/acceptance/HomepageCept.php
@@ -3,4 +3,9 @@ $I = new AcceptanceTester($scenario);
 $I->wantTo('ensure that frontpage works');
 $I->amOnPage('/');
 $I->see('Login');
+$I->fillField('name', 'tester');
+$I->fillField('password', 'tester');
+$I->click('login');
+$I->amOnPage('/index.php');
+$I->see('My List');
 ?>
